afde6df3c9a94e0931db1557b40cb4ba6c6b6d0a
EC2 Default User <ec2-user@ip-172-31-28-204.ap-northeast-1.compute.internal>
2022-11-01 13:56:58 +0000

    再度電卓について調べ直し、コードを一新してみました。

--- calculator.js
+++ calculator.js
@@ -1,16 +1,50 @@
-    function get_calc(btn) {
-      if(btn.value == "=") {
-        document.dentaku.display.value = eval(document.dentaku.display.value);
-      } else if (btn.value == "AC") {
-        document.dentaku.display.value = "";
-      } else {
-        if (btn.value == "*") {
-          btn.value = "*";
-        } else if (btn.value == "/") {
-          btn.value = "/";
-        } 
-        document.dentaku.display.value += btn.value;
-        document.dentaku.add_btn.value = "×";
-        document.dentaku.div_btn.value = "÷";
-      }
+let result = document.getElementById("result");
+let data ="";
+let is_calc = false;
+
+function ope_click(val){
+  if(is_calc) is_calc = false;
+  
+  if(is_ope_last()){
+    result.value = result.value.slice(0,-1) + val;
+  }else{
+    result.value += val;
+  }
+}
+
+function is_ope_last(){
+  return ["+","-","*","/"].includes(result.value.toString().slice(-1));
+}
+
+function num_click(value) {
+  if(is_calc) result.value = "0";
+  is_calc = false;
+  
+  if(result.value == "0" && value =="0"){
+    result.value ="0";
+  }else if(result.value == "0" && value =="."){
+    result.value ="0.";
+  }else if(result.value == "0"){
+    result.value = value;
+  }else{
+    result.value += value;
+  }
+}
+
+
+function equal_click(){
+    if(is_ope_last()) result.value.slice(0,-1);
+    result.value = new Function("return " + result.value)();
+    
+    if (ans == Infinity || Number.isNaN(ans)){
+        result.value = "Error";
+    }else{
+        result.value = ans;
+        is_calc = true;
     }
+}
+
+function c_click(){
+  result.value = "0";
+  is_clac = false;
+}
